18e27f5b3f3c25bbc02e75720f1a30d9333bb53a
Your Name <wilkinsnigel2000@gmail.com>
2023-08-06 21:38:16 -0700

    deleted!

--- src/app/services/skill-data.service.ts
+++ src/app/services/skill-data.service.ts
@@ -10,127 +10,117 @@ export class SkillDataService {
   dev=[
     {
       name:'Angular',
-      img_src:'/assets/angular.svg',
-    },
-    {
-      name:'Javascript',
-      img_src:'/assets/js.svg',
-    },
-    {
-      name:'Typescript',
-      img_src:'/assets/typescript.svg',
+      img_src:'https://i.imgur.com/RZADRRW.png',
     },
     {
       name:'Node.Js',
-      img_src:'/assets/nodejs.svg',
+      img_src:'https://i.imgur.com/7lzMo8T.png',
+    },
+    {
+      name:'Typescript',
+      img_src:'https://i.imgur.com/dECq6xj.png',
+    },
+    {
+      name:'Javascript',
+      img_src:'https://i.imgur.com/sebEcQT.png',
     },
     {
       name:'CSS',
-      img_src:'/assets/css.svg',
+      img_src:'https://i.imgur.com/MRssf9Z.png',
     },
     {
       name:'HTML5',
-      img_src:'/assets/html5.svg',
+      img_src:'https://i.imgur.com/y0EE3OX.png',
     },
     {
       name:'Material UI',
-      img_src:'/assets/mui.svg',
+      img_src:'https://i.imgur.com/saS7OKX.png',
     },
     {
       name:'Postman',
-      img_src:'/assets/postman.svg',
+      img_src:'https://i.imgur.com/IIwO7wD.png',
     },
     {
       name:'React',
-      img_src:'/assets/react.svg',
+      img_src:'https://i.imgur.com/mqJJnWN.png',
     },
     {
       name:'TailwindCSS',
-      img_src:'/assets/tailwindcss.svg',
+      img_src:'https://i.imgur.com/M46w1bh.png',
     },
     {
       name:'Unity',
-      img_src:'/assets/unity.svg',
+      img_src:'https://i.imgur.com/YpC5CSq.png',
     },
     {
       name:'Visual Studio',
-      img_src:'/assets/vscode.svg',
+      img_src:'https://i.imgur.com/01yDaXh.png',
     },
     {
       name:'C#',
-      img_src:'/assets/csharp.svg',
+      img_src:'https://i.imgur.com/SloTg3Z.png',
     },
     {
       name:'Bootstrap',
-      img_src:'/assets/bootstrap.svg',
+      img_src:'https://i.imgur.com/iGSrx9j.png',
     },
     {
       name:'Angular',
-      img_src:'/assets/angular.svg',
+      img_src:'https://i.imgur.com/RZADRRW.png',
     },
-    {
-      name:'Angular',
-      img_src:'/assets/angular.svg',
-    },
-    
-    
     
   ]
   work_flow=[
     {
       name:'Apple',
-      img_src:'/assets/apple.svg',
+      img_src:'https://i.imgur.com/Ouk9QjH.png',
     },
     {
       name:'Azure',
-      img_src:'/assets/azure.svg',
+      img_src:'https://i.imgur.com/an2nhpm.png',
     },
     {
       name:'Jira',
-      img_src:'/assets/jira.svg',
+      img_src:'https://i.imgur.com/jw6HxQ0.png',
     },
     {
       name:'Notion',
-      img_src:'/assets/notion.svg',
+      img_src:'https://i.imgur.com/zv1sggK.png',
     },
     {
       name:'Postman',
-      img_src:'/assets/postman.svg',
+      img_src:'https://i.imgur.com/IIwO7wD.png',
     },
     {
       name:'Slack',
-      img_src:'/assets/angular.svg',
+      img_src:'https://i.imgur.com/ss6b89k.png',
     },
     {
       name:'Windows',
-      img_src:'/assets/windows.svg',
+      img_src:'https://i.imgur.com/gtKmiv6.png',
     },
     {
       name:'Filezilla',
-      img_src:'/assets/filezilla.svg',
-    },
-    {
-      name:'Github',
-      img_src:'/assets/github.svg',
+      img_src:'https://i.imgur.com/TRqRHrt.png',
     },
     
   ]
   ui_ux=[
     {
       name:'Adobe Photoshop',
-      img_src:'assets/photoshop.svg',
+      img_src:'https://i.imgur.com/pLOMnDp.png',
     },
     {
       name:'Adobe Illustrator',
-      img_src:'assets/illustrator.svg',
+      img_src:'https://i.imgur.com/WSjjtLg.png',
     },
     {
       name:'Adobe InDesign',
-      img_src:'assets/photoshop.svg',
+      img_src:'https://i.imgur.com/WSjjtLg.png',
     },
     {
       name:'Figma',
-      img_src:'/assets/figma.svg',
+      img_src:'https://i.imgur.com/aBpFwNp.png',
     },
   ]
 
