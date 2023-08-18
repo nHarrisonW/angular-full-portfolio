42c769d32097e57906075a9f8b88f5a452c083a3
nHarrisonW <wilkinsnigel2000@gmail.com>
2023-08-18 16:49:46 -0700

    go home!

--- src/app/services/letters.service.ts
+++ src/app/services/letters.service.ts
@@ -1,36 +1,40 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LettersService {
+  constructor() {}
 
-  constructor() { }
-
-  letters=[
+  letters = [
     {
-      route:'costco',
-      src:'https://i.imgur.com/anKLobh.jpg',
-      name:'Inez Aravelo',
-      html:`
+      reference: 'assets/InezALetter.pdf',
+      route: 'costco',
+      src: 'https://i.imgur.com/anKLobh.jpg',
+      name: 'Inez Aravelo',
+      html: `
+      <div>
+        
+      </div>
+      `,
+    },
+    {
+      reference: 'assets/JasmineLLetter.pdf',
+      route: 'inshape',
+      src: 'https://i.imgur.com/xOy9enk.jpg',
+      name: 'Jasmine Lee',
+      html: `
       <img src="" class="letter">
       `,
     },
     {
-      route:'inshape',
-      src:'https://i.imgur.com/xOy9enk.jpg',
-      name:'Jasmine Lee',
-      html:`
+      reference: 'assets/isaiahFLetter.pdf',
+      route: 'csa',
+      src: 'https://i.imgur.com/l2mXvtR.jpg',
+      name: 'Isaiah Ferguson',
+      html: `
       <img src="" class="letter">
       `,
     },
-    {
-      route:'csa',
-      src:'https://i.imgur.com/l2mXvtR.jpg',
-      name:'Isaiah Ferguson',
-      html:`
-      <img src="" class="letter">
-      `,
-    },
-  ]
+  ];
 }
