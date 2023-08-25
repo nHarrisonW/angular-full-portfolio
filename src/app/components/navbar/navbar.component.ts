e0db44bddededef3d532271baf8d86f8a250da00
nHarrisonW <wilkinsnigel2000@gmail.com>
2023-08-25 16:52:41 -0700

    go home!!!!

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,14 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { DarkLightService } from 'src/app/services/dark-light.service';
+
+
+interface Route {
+  label: string;
+  routerLink: string;
+  active: boolean;
+  hidden:boolean;
+}
 
 @Component({
   selector: 'app-navbar',
@@ -8,5 +17,28 @@ import { DarkLightService } from 'src/app/services/dark-light.service';
 })
 export class NavbarComponent {
   @Input() projectsData!: any[]; 
-  constructor(public darkLightService:DarkLightService){}
+  constructor(public darkLightService:DarkLightService, private router:Router){}
+
+  routes: Route[] = [
+    { label: 'Home', routerLink: '', active: true, hidden: false },
+    { label: 'Stockton Mayor Website', routerLink: 'project/StocktonMayorWebsite', active: false, hidden: true },
+    { label: 'CORSACRAFT', routerLink: 'project/CORSACRAFT', active: false, hidden: true },
+    { label: 'SuppWorld!', routerLink: 'project/SuppWorld', active: false, hidden: true },
+    { label: 'Angular Password Generator', routerLink: 'project/NgPasswordGenerator', active: false, hidden: true },
+    { label: 'About Me', routerLink: 'about/me', active: false, hidden: true },
+  ];
+
+  
+  toggleRoutes(){
+    this.routes.forEach(route => {
+      if(!route.active){
+        route.hidden=!route.hidden;
+      }
+    })
+  }
+  navigate(routerlink:any){
+    this.router.navigate(routerlink)
+  }
+
+
 }
